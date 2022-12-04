6c2fd4a775e1ae1a11095818298cfbfce8eb0f9d
Adam Schlinker <adam.schlinker@uleth.ca>
2022-12-03 20:15:30 -0700

    initial commit

---
+++ MainWindow.cpp
@@ -0,0 +1,23 @@
+#include "MainWindow.h"
+#include "./ui_MainWindow.h"
+
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->pushButton->setText("Clicked");
+}
+
